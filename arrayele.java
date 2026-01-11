0161cbc7793f1e1eae56d1e130b339d0683224c0
deekshadsm <deekshasm88@gmail.com>
2026-01-11 23:14:37 +0530

    arrayelement-sum

---
+++ arrayele.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[5];
+        int sum = 0;
+
+        System.out.println("Enter 5 elements:");
+
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.println("Array elements are:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println("\nSum of array elements: " + sum);
+
+        sc.close();
+    }
+}
